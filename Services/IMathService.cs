ca809480f27d5bb20bc7db9d333fe3a85d6efcc5
EugeneStv <mrevgen010@gmail.com>
2025-01-21 02:05:50 +0700

    Add files via upload

---
+++ Services/IMathService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Services
+{
+    public interface IMathService
+
+    {
+        int FirstNumber { get; }
+        int SecondNumber { get; }
+        int Sum();
+        int Sub();
+        int Mul();
+        int Div();
+
+    }
+}
---
+++ Services/MathService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Services
+{
+    public class MathService : IMathService
+    {
+        public int FirstNumber { get; private set; }
+        public int SecondNumber { get; private set; }
+
+        public MathService()
+        {
+            Random random = new Random();
+            (FirstNumber, SecondNumber) = (random.Next(11), random.Next(11));
+        }
+
+        public int Sum()
+        {
+            return FirstNumber + SecondNumber;
+        }
+
+        public int Mul()
+        {
+            return FirstNumber * SecondNumber;
+        }
+
+        public int Sub()
+        {
+            return FirstNumber - SecondNumber;
+        }
+
+        public int Div()
+        {
+            try
+            {
+                var divResult = FirstNumber / SecondNumber;
+                return divResult;
+            }
+            catch (DivideByZeroException)
+            {
+                return -1;
+            }
+        }
+    }
+}
